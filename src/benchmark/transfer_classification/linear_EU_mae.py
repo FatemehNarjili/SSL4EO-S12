f1037b37764a95bb7bcbc1b49e787af271b910ae
Ubuntu <ubuntu@ip-172-31-0-117.eu-central-1.compute.internal>
2025-08-10 07:49:38 +0000

    Save last and best model checkpoint

--- src/benchmark/transfer_classification/linear_EU_mae.py
+++ src/benchmark/transfer_classification/linear_EU_mae.py
@@ -318,10 +318,10 @@ def main(args):
             log_writer=log_writer,
             args=args
         )
-        if args.output_dir and (epoch%10==0 or epoch + 1 == args.epochs):
-            misc.save_model(
-                args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-                loss_scaler=loss_scaler, epoch=epoch)
+
+        misc.save_model(
+            args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
+            loss_scaler=loss_scaler, epoch="last")
 
         # if epoch%5==0 or (epoch + 1 == args.epochs):
         test_stats = evaluate(data_loader_val, model, device, criterion)
@@ -330,7 +330,7 @@ def main(args):
         if test_stats["acc1"] > max_accuracy:
             misc.save_model(
             args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-            loss_scaler=loss_scaler, epoch=epoch)
+            loss_scaler=loss_scaler, epoch="best")
             
         max_accuracy = max(max_accuracy, test_stats["acc1"])
         print(f'Max accuracy: {max_accuracy:.2f}%')
